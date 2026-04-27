36b62b9e22b73b1abbf7147394843ed9a60aa035
itsMilanCodesu <milantamang350359@gmail.com>
2026-04-27 21:58:00 +0900

    ques : medium question of operator solved (5)

--- 2(operators)/mediumQues.js
+++ 2(operators)/mediumQues.js
@@ -1,32 +1,74 @@
-// Ques 1: 
+// // Ques 1: 
 
-let a = 5;
-let b = a++ + ++a;
+// let a = 5;
+// let b = a++ + ++a;
     
-console.log(b);
+// console.log(b);
 
-// ans is 12
+// // ans is 12
 
-/* Explaination
+// /* Explaination
 
- a = 5
+//  a = 5
 
-step : 1
- b = a++ , here value of b is 5 and value of a is increased which became a = 6
- b = 5 + ++a
+// step : 1
+//  b = a++ , here value of b is 5 and value of a is increased which became a = 6
+//  b = 5 + ++a
 
-step : 2
+// step : 2
 
-in step 2 value of a is 6 so ++a became 7 , a = 7 
-b = 5 + 7 
-b = 12
-
-
-
-console.log(b) = 12
+// in step 2 value of a is 6 so ++a became 7 , a = 7 
+// b = 5 + 7 
+// b = 12
 
 
 
+// console.log(b) = 12
 
 
-*/
+
+
+
+// */
+
+
+
+
+
+
+
+// // Ques 2 : 
+
+// // 7. What happens here?
+// console.log(0 == false); // ans is   true
+// console.log(0 === false);  // ans is false because type of 0 is number and false i boolean
+
+
+
+// // Ques 3 : 
+//  // predict the ans
+
+// let x = 10;
+// x /= 2 + 3;  // ans is 2 
+// console.log("ans i s" , x);
+
+
+
+// // Ques 4 
+
+// let n = null;
+// let u = undefined;
+// console.log(n == u); // true
+// console.log(n === u);  // false 
+
+// Ques 5 
+
+// What is printed 
+
+// let x = 5;
+// console.log(x > 3 ? x++ : x--); // ans is 5
+// console.log(x); // ans is 6
+
+
+ 
+
