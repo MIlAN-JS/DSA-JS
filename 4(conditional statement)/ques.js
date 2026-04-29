aa33878d9d6f9dbdad7a65ade29d3a456db7df58
itsMilanCodesu <milantamang350359@gmail.com>
2026-04-29 21:31:27 +0900

    ques : solved conditional ques one ques

---
+++ 4(conditional statement)/ques.js
@@ -0,0 +1,13 @@
+//1.  accept an integer and check whether it is even or odd number
+
+// const prompt = require("prompt-sync")();
+
+
+// let num = prompt("Enter the number : ");
+
+
+// if (num%2 == 0 )console.log("given number" + num + "is even");
+// else console.log("given number" + num + "is odd");
+
+
+2. 
