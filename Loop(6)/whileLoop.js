655248a5415932cda4afe055b539b828ef413563
itsMilanCodesu <milantamang350359@gmail.com>
2026-07-23 06:54:01 +0900

    add : second whileloop ques

--- Loop(6)/whileLoop.js
+++ Loop(6)/whileLoop.js
@@ -8,13 +8,30 @@ let n = +prompt("Enter number")
 // question : 1  (print the given numbers seperately)
 
 
-
-
-while(n>0){
+// while(n>0){
     
-    lDigit = n % 10 ; 
-     console.log(lDigit , "digit is ");
-     n = Math.floor( n /10)
-     console.log(n , "n is ") 
+//     lDigit = n % 10 ; 
+//      console.log(lDigit , "digit is ");
+//      n = Math.floor( n /10)
+//      console.log(n , "n is ") 
+
+// }
+
+
+//question : 2  reverse the given n number
+
+let rev = 0 
+while (n> 0){
+
+    let rem = n % 10 ; 
+     rev = (rev *10) + rem // multiplied with 10 cause we dont want to loose the last value 
+
+      n = Math.floor(n/10);
+
+    
 
 }
+
+
+console.log(rev)
+
